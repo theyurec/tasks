f73ab269066a4d4520eb594b69031ada0a971236
Yuriy <the.yurec1@yandex.ru>
2022-04-09 11:42:35 +0300

    task2

--- task2.js
+++ task2.js
@@ -9,13 +9,9 @@ function wordsToMarks(word){
     let obj = {a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16, q: 17, r: 18,
         s: 19, t: 20, u: 21, v: 22, w: 23, x: 24, y: 25, z: 26};
     let sum = 0;
-    for(let key in obj){
         for(let i = 0; i <= word.length; i++){
-            if(word[i] == key){
-                sum += obj[key]
-            }
+            if(word[i] in obj) sum += obj[word[i]]
         }
-    }
    console.log(sum)
 }
-wordsToMarks('knowledge')
+wordsToMarks('love')
